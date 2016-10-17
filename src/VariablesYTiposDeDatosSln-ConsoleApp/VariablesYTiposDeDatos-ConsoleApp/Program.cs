53086c771c5ffba4ad09adffce4682402944d55e
Eduardo Fonseca <efonsecab@gmail.com>
2016-10-17 11:44:20 -0600

    Agregando ejemplo de variables y tipos de datos

---
+++ src/VariablesYTiposDeDatosSln-ConsoleApp/VariablesYTiposDeDatos-ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesYTiposDeDatos_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool nombreVariable;
+            bool nombreVariable2 = false;
+            bool boolVariable = false;
+            int intVariable = 55;
+            long longVariable = 5465;
+            float floatVariable = 45;
+            floatVariable = 13.4f;
+
+            double doubleVariable = 45.6;
+            decimal decimalVariable = 45.6M;
+            char charVariable = '8';
+            string stringVariable = "mi hilera%$^$%&%^*%$%#sdfsdf";
+            string stringVariable2 = "mi hilera 2";
+            stringVariable2 = stringVariable;
+            byte byteVariable = 255;
+        }
+    }
+}
