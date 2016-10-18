eb2e210c52190e9268d3833060d12f86954d2569
Eduardo Fonseca <efonsecab@gmail.com>
2016-10-17 18:27:47 -0600

    agregando ejemplo de ciclos

--- src/EstructurasDeDatosSln-ConsoleApp/EstructurasDeDatos-ConsoleApp/Program.cs
+++ src/EstructurasDeDatosSln-ConsoleApp/EstructurasDeDatos-ConsoleApp/Program.cs
@@ -19,13 +19,13 @@ namespace EstructurasDeDatos_ConsoleApp
             numArray[4] = 21;
             numArray[5] = 6;
             numArray[6] = 98;
-            Imprimir(0, numArray);
-            Imprimir(1, numArray);
-            Imprimir(2, numArray);
-            Imprimir(3, numArray);
-            Imprimir(4, numArray);
-            Imprimir(5, numArray);
-            Imprimir(6, numArray);
+            //Imprimir(0, numArray);
+            //Imprimir(1, numArray);
+            //Imprimir(2, numArray);
+            //Imprimir(3, numArray);
+            //Imprimir(4, numArray);
+            //Imprimir(5, numArray);
+            //Imprimir(6, numArray);
             int[,] numMatrix = new int[3,3];
             numMatrix[0, 0] = 5;
             numMatrix[0, 1] = 15;
@@ -38,17 +38,52 @@ namespace EstructurasDeDatos_ConsoleApp
             numMatrix[2, 0] = 65;
             numMatrix[2, 1] = 165;
             numMatrix[2, 2] = 28;
-            Imprimir(0, 0, numMatrix);
-            Imprimir(0, 1, numMatrix);
-            Imprimir(0, 2, numMatrix);
+            //Imprimir(0, 0, numMatrix);
+            //Imprimir(0, 1, numMatrix);
+            //Imprimir(0, 2, numMatrix);
 
-            Imprimir(1, 0, numMatrix);
-            Imprimir(1, 1, numMatrix);
-            Imprimir(1, 2, numMatrix);
+            //Imprimir(1, 0, numMatrix);
+            //Imprimir(1, 1, numMatrix);
+            //Imprimir(1, 2, numMatrix);
 
-            Imprimir(2, 0, numMatrix);
-            Imprimir(2, 1, numMatrix);
-            Imprimir(2, 2, numMatrix);
+            //Imprimir(2, 0, numMatrix);
+            //Imprimir(2, 1, numMatrix);
+            //Imprimir(2, 2, numMatrix);
+
+            //Uso de ciclos
+            int posEnArray = 0;
+            while (posEnArray < numArray.Length)
+            {
+                Imprimir(posEnArray, numArray);
+                posEnArray = posEnArray + 1;
+            }
+
+            for (int iPos = 0; iPos < numArray.Length; iPos++)
+            {
+                Imprimir(iPos, numArray);
+            }
+
+            foreach (int valor in numArray)
+            {
+                Console.WriteLine("Valor=" + valor);
+            }
+
+            Console.WriteLine("Ejemplo do-while");
+            posEnArray = 0;
+            do
+            {
+                Imprimir(posEnArray, numArray);
+                posEnArray++;
+            } while (posEnArray < numArray.Length);
+
+            Console.WriteLine("Ejemplo for(matriz)");
+            for (int f=0; f < numMatrix.GetLength(0); f++)
+            {
+                for (int c = 0; c < numMatrix.GetLength(1); c++)
+                {
+                    Imprimir(f, c, numMatrix);
+                }
+            }
             Console.WriteLine("Presiona una tecla para salir");
             Console.ReadKey();
         }
