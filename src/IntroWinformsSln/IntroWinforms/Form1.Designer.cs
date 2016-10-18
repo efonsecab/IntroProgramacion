a9e5083352bb47d19def580597bf405d4e790a1f
Eduardo Fonseca <efonsecab@gmail.com>
2016-10-18 08:52:40 -0600

    Pequeña intro a Windows Forms

---
+++ src/IntroWinformsSln/IntroWinforms/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace IntroWinforms
+{
+    partial class frmFormPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(12, 25);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(86, 13);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Digite su nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(104, 22);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(165, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(15, 51);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(75, 23);
+            this.btnMostrar.TabIndex = 2;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // frmFormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(990, 558);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Name = "frmFormPrincipal";
+            this.Text = "Intro a Windows Forms";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnMostrar;
+    }
+}
+
---
+++ src/IntroWinformsSln/IntroWinforms/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Ver https://msdn.microsoft.com/es-cr/library/dd30h2yb(v=vs.110).aspx
+//Ver https://msdn.microsoft.com/en-us/library/system.windows.forms(v=vs.110).aspx
+namespace IntroWinforms
+{
+    public partial class frmFormPrincipal : Form
+    {
+        public frmFormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            string nombreAMostrar = txtNombre.Text;
+            DialogResult result = MessageBox.Show("¿Quiere mostrar el texto digitado?", "¿Continuar?", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+                MessageBox.Show("El nombre digitado es: " + nombreAMostrar, "Nombre digitado");
+            else
+                MessageBox.Show("Usted decidió no mostrar el nombre");
+        }
+    }
+}
